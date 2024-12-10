4100e444d6b772b851164c7f0d83956ca6003894
pesch828 <p.esch@jcsds.net>
2024-12-10 23:17:42 +0000

    Test class

--- project/main.cpp
+++ project/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include <limits>
 
+class Test
+{
+    private:
+        /* data */
+        void say_name();
+    public:
+        Test(/* args */);
+        ~Test();
+    };
+
+    // Constructor
+    Test::Test(/* args */)
+    {
+    }
+
+    // Deconstructor
+    Test::~Test()
+    {
+}
+
+void Test::say_name() {
+    std::cout << "I'm the test class";
+}
+
 int main() {
     std::cout << "Hello, world!\n";
 
